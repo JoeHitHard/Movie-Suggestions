4de792b8a5221743abda104f4fc61a2264040e11
Joseph Hit Hard <joseph.meghanath@gmail.com>
2024-05-09 17:37:18 -0500

    added code to display graph

--- movie-suggestions.py
+++ movie-suggestions.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
@@ -28,3 +27,28 @@ def recommend(title, cosine_sim=cosine_sim):
 
 recommendations = recommend('Little Things')
 print(recommendations)
+
+import plotly.graph_objects as go
+
+# Get the cosine similarity scores for the first 10 movies
+number_of_movies = 100
+scores = cosine_sim[:number_of_movies, :number_of_movies]
+
+score_data = []
+for i in range(number_of_movies):
+    scores[i][i] = 0
+    score_data.append(
+        go.Heatmap(z=scores, x=list(df['title'][:number_of_movies]), y=list(df['title'][:number_of_movies]),
+                   name=df['title'][i], colorscale='Greys'))
+
+# Create a bar chart
+fig = go.Figure(data=score_data)
+
+# Update layout
+fig.update_layout(title='Cosine Similarity Scores of First ' + str(number_of_movies) + ' Movies', xaxis_title='Movie Title',
+                  yaxis_title='Cosine Similarity Score')
+# Convert the plot to HTML
+graph_html = fig.to_html()
+
+with open('cosine_similarity_' + str(number_of_movies) + '.html', 'w') as f:
+    f.write(graph_html)
